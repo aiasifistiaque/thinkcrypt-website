8f6f18f1062656cbb8b89353ca255e9aceb6092a
Asif Istiaque <asifistiaque.ai@gmail.com>
2024-04-20 02:10:00 +0600

    sd

--- components/scroll/Scroll.js
+++ components/scroll/Scroll.js
@@ -1,14 +1,11 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useRef } from 'react';
 import { Flex } from '@chakra-ui/react';
-import { motion, useTransform } from 'framer-motion';
-import { LocomotiveScrollProvider } from 'react-locomotive-scroll';
 
 const Scroll = ({ children }) => {
 	const containerRef = useRef(null);
 
 	return (
 		<Flex
-			as={LocomotiveScrollProvider}
 			style={{ zIndex: 99 }}
 			options={{
 				multiplier: 0.9,
@@ -16,14 +13,7 @@ const Scroll = ({ children }) => {
 				smooth: false,
 				// ... all available Locomotive Scroll instance options
 			}}
-			containerRef={containerRef}>
-			<Flex
-				zIndex={99}
-				data-scroll-container
-				ref={containerRef}>
-				{children}
-			</Flex>
-		</Flex>
+			containerRef={containerRef}></Flex>
 	);
 };
 
