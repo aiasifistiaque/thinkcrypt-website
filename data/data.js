8f36775c001c10cecc986898a1f4a3acb537358f
Asif Istiaque <asifistiaque.ai@gmail.com>
2024-04-20 19:34:46 +0600

    sd

--- data/data.js
+++ data/data.js
@@ -4,7 +4,7 @@ export const howWeWorkPic2 = '/images/howWeWork2.jpg';
 export const howWeWork = [
 	{
 		id: '1',
-		title: '03 Website Design',
+		title: '01 Website Design',
 		subTitle:
 			'Our website designs are contemporary with a modern outlook which will seize your customers’ attention. It will help you convey your message and of course, sell more. We worry about the maintenance of your website so you don’t have to. ',
 	},
@@ -16,20 +16,20 @@ export const howWeWork = [
 	},
 	{
 		id: '3',
-		title: '05 Prototyping for Startups',
+		title: '03 Prototyping for Startups',
 		subTitle:
 			'For startups, we have special packages where we build and test your desired digital products. You give us the brief and we assist you through your prototypes. Simply and efficiently',
 	},
 	{
 		id: '4',
-		title: '01 UI/UX Solution',
+		title: '04 UI/UX Solution',
 		subTitle:
 			'Our experienced team can handle every problem with your apps and delivery them with smart solutions. We assist so you can excel. We are the one-stop platform for your every digital query.',
 	},
 
 	{
 		id: '5',
-		title: '04 Content Solution',
+		title: '05 Content Solution',
 		subTitle:
 			'We also provide you with regular content writing for your business. Build the perfect brand which reveals your unmatched personality. Put your thoughts into carefully curated words with the help of our team’s expertise.',
 	},
