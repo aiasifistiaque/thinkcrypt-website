36f8440628e1ff72cc601177b61f9dd61ec7820d
arefin.aareef <arefin.aareef@gmail.com>
2024-04-21 12:12:30 +0600

    our services section fixed

--- components/home/services/ServicesNew.jsx
+++ components/home/services/ServicesNew.jsx
@@ -51,159 +51,133 @@ const ServicesNew = () => {
 	};
 
 	return (
-		<Container spacing={0} >
+		// <Container spacing={0} >
+		<Column
+			px={{ base: '0px', md: '24px' }}
+			pr={{ base: '0px', md: '128px' }}
+			w='full'
+			mx='auto'
+			// maxW={layout.MAX_W}
+			borderTop={BORDER}
+			borderBottom={BORDER}
+			mt='60px'
+			mb='40px'
+		>
+			{/* FOR SMALL SCREEN */}
+			<Column display={{ base: 'flex', md: 'none' }}>
+				<Grid
+					templateRows='2fr 1fr'
+					display={{ base: 'grid', md: 'none' }}
+					w='full'
+				>
+					<Grid templateColumns='1fr 1fr' w='full'>
+						<CustomImage w='full' h='324px'>
+							{howWeWorkPic1}
+						</CustomImage>
+						<Flex w='full' p={4}>
+							<Text fontSize='32px' fontFamily={fonts.heading}>
+								Our Services
+							</Text>
+						</Flex>
+					</Grid>
+					<CustomImage w='full' h='full'>
+						{howWeWorkPic2}
+					</CustomImage>
+				</Grid>
+
+				<Column ml={6}>
+					<Accordion defaultIndex={[0]} borderLeft={BORDER}>
+						{howWeWork?.map((item, index) => (
+							<AccordionItem key={index} border={BORDER}>
+								<h2>
+									<AccordionButton>
+										<Box as='span' flex='1' textAlign='left' py={3}>
+											<Text
+												fontSize='18px'
+												fontWeight={600}
+												fontFamily={fonts.heading}
+											>
+												{item.title}
+											</Text>
+										</Box>
+									</AccordionButton>
+								</h2>
+								<AccordionPanel pb={4}>
+									<Text fontSize='14px' fontFamily='Suisse'>
+										{item.subTitle}
+									</Text>
+								</AccordionPanel>
+							</AccordionItem>
+						))}
+					</Accordion>
+				</Column>
+			</Column>
+
+			{/* FOR LARGE SCREEN */}
 			<Column
-				px={{lg:'128px'}}
-				w='full'
-				mx='auto'
-				maxW={layout.MAX_W}
-				borderTop={BORDER}
-				borderBottom={BORDER}
-				mt='40px'>
-				{/* FOR SMALL SCREEN */}
-				<Column display={{ base: 'flex', md: 'none' }}>
-					<Grid
-						templateRows='2fr 1fr'
-						display={{ base: 'grid', md: 'none' }}
-						w='full'>
-						<Grid
-							templateColumns='1fr 1fr'
-							w='full'>
-							<CustomImage
-								w='full'
-								h='324px'>
-								{howWeWorkPic1}
-							</CustomImage>
-							<Flex
-								w='full'
-								p={4}>
-								<Text
-									fontSize='32px'
-									fontFamily={fonts.heading}>
-									Our Services
-								</Text>
-							</Flex>
-						</Grid>
-						<CustomImage
-							w='full'
-							h='full'>
+				display={{ base: 'none', md: 'flex' }}
+				borderLeft={BORDER}
+				borderRight={BORDER}
+			>
+				<Grid templateColumns='2fr 3fr' w='full'>
+					<CustomImage w='full' h='full'>
+						{howWeWorkPic1}
+					</CustomImage>
+					<Grid templateRows='1fr 1fr' w='full'>
+						<Flex w='full' p={8}>
+							<Text fontSize='48px' fontFamily={fonts.heading} fontWeight='600'>
+								Our Services
+							</Text>
+						</Flex>
+						<CustomImage w='full' h='full'>
 							{howWeWorkPic2}
 						</CustomImage>
 					</Grid>
+				</Grid>
 
-					<Column ml={6}>
-						<Accordion
-							defaultIndex={[0]}
-							borderLeft={BORDER}>
-							{howWeWork?.map((item, index) => (
-								<AccordionItem key={index} border={BORDER}>
-									<h2>
-										<AccordionButton>
-											<Box
-												as='span'
-												flex='1'
-												textAlign='left'
-												py={3}>
-												<Text
-													fontSize='18px'
-													fontWeight={600}
-													fontFamily={fonts.heading}>
-													{item.title}
-												</Text>
-											</Box>
-										</AccordionButton>
-									</h2>
-									<AccordionPanel pb={4}>
-										<Text
-											fontSize='14px'
-											fontFamily='Suisse'>
-											{item.subTitle}
-										</Text>
-									</AccordionPanel>
-								</AccordionItem>
-							))}
-						</Accordion>
-					</Column>
-				</Column>
-
-				{/* FOR LARGE SCREEN */}
-				<Column
-					display={{ base: 'none', md: 'flex' }}
-					borderLeft={BORDER}
-					borderRight={BORDER}>
-					<Grid
-						templateColumns='2fr 3fr'
-						w='full'>
-						<CustomImage
-							w='full'
-							h='full'>
-							{howWeWorkPic1}
-						</CustomImage>
-						<Grid
-							templateRows='1fr 1fr'
-							w='full'>
-							<Flex
-								w='full'
-								p={8}>
-								<Text
-									fontSize='48px'
-									fontFamily={fonts.heading}
-									fontWeight='600'>
-									Our Services
-								</Text>
-							</Flex>
-							<CustomImage
-								w='full'
-								h='full'>
-								{howWeWorkPic2}
-							</CustomImage>
-						</Grid>
-					</Grid>
-
-					<Grid
-						w='full'
-						templateColumns='repeat(4, 1fr)'
-						gap='1px'
-						bgColor='#DEDEE0'>
-						{howWeWork?.map((item, index) => (
-							<Column
-								key={index}
-								px={4}
-								py={6}
-								gap={5}
-								bgColor='white'
-								h='230px'
-								transition='all 0.3s'
-								// onMouseEnter={() => setHoveredIndex(index)}
-								// onMouseLeave={() => setHoveredIndex(0)}
-								gridColumn={
-									index === 0
-										? 'span 2'
-										: index === 4 && hoveredIndex === 3
-										? '3'
-										: 'auto'
-								}
-								gridRow={ index === 4 && hoveredIndex === 3 ? '1' : 'auto' }
+				<Grid
+					w='full'
+					templateColumns='repeat(4, 1fr)'
+					gap='1px'
+					bgColor='#DEDEE0'
+				>
+					{howWeWork?.map((item, index) => (
+						<Column
+							key={index}
+							px={4}
+							py={6}
+							gap={5}
+							bgColor='white'
+							h='230px'
+							transition='all 0.3s'
+							// onMouseEnter={() => setHoveredIndex(index)}
+							// onMouseLeave={() => setHoveredIndex(0)}
+							gridColumn={
+								index === 0
+									? 'span 2'
+									: index === 4 && hoveredIndex === 3
+									? '3'
+									: 'auto'
+							}
+							gridRow={index === 4 && hoveredIndex === 3 ? '1' : 'auto'}
+						>
+							<Text fontSize='24px' fontWeight='400' fontFamily={fonts.heading}>
+								{item.title}
+							</Text>
+							<Text
+								fontFamily='Suisse'
+								fontSize='16px'
+								fontWeight='400'
+								overflow='hidden'
 							>
-								<Text
-									fontSize='24px'
-									fontWeight='400'
-									fontFamily={fonts.heading}>
-									{item.title}
-								</Text>
-								<Text
-									fontFamily='Suisse'
-									fontSize='16px'
-									fontWeight='400'
-									overflow='hidden'>
-									{item.subTitle}
-								</Text>
-							</Column>
-						))}
-					</Grid>
-				</Column>
+								{item.subTitle}
+							</Text>
+						</Column>
+					))}
+				</Grid>
 			</Column>
-		</Container>
+		</Column>
+		// </Container>
 	);
 };
 
