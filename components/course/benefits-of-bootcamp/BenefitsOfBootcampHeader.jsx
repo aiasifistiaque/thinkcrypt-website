10e8b697d7ac4624e2a07921267d40a65b194a17
ashikuzzaman-abir <ashikuzzamanabir@outlook.com>
2024-05-21 17:28:53 +0600

    updone benefits section

---
+++ components/course/benefits-of-bootcamp/BenefitsOfBootcampHeader.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import BorderContainer from '../../util/container/BorderContainer.js'
+
+const BenefitsOfBootcampHeader = () => {
+  return (
+    <BorderContainer>
+      
+    </BorderContainer>
+  )
+}
+
+export default BenefitsOfBootcampHeader
---
+++ components/course/benefits-of-bootcamp/BenefitsOfBootcampSection.jsx
@@ -0,0 +1,10 @@
+import { Stack } from '@chakra-ui/react'
+import React from 'react'
+
+const BenefitsOfBootcampSection = () => {
+  return <Stack w='full' spacing={6}>
+    
+    </Stack>;
+}
+
+export default BenefitsOfBootcampSection
