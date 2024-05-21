531a7275590961d5781736086899551326845659
Md Aminul Haque <61130539+AminulHaqueShihab@users.noreply.github.com>
2024-05-21 15:52:48 +0600

    merge with main

--- components/course/page/SingleCoursePage.jsx
+++ components/course/page/SingleCoursePage.jsx
@@ -1,9 +1,4 @@
 import React from 'react';
-import Page from '../../util/Page/Page';
-import Head from 'next/head';
-import CircleFollow from '../../util/CircleFollow';
-import Header from '../../util/Header/Header';
-import Scroll from '../../scroll/Scroll';
 import BootcampScheduleSection from '../bootcamp-schedule/BootcampScheduleSection.jsx';
 import { courseData } from '../../../lib/courseData.js';
 import PageLayout from '../../util/Page/PageLayout.jsx';
