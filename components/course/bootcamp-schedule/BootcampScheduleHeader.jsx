eac25a84e531f03a4136a18450248ea042c67eac
ashikuzzaman-abir <ashikuzzamanabir@outlook.com>
2024-05-21 14:53:58 +0600

    update

--- components/course/bootcamp-schedule/BootcampScheduleHeader.jsx
+++ components/course/bootcamp-schedule/BootcampScheduleHeader.jsx
@@ -1,11 +1,19 @@
 import { Stack } from '@chakra-ui/react';
 import React from 'react';
 import HeaderText from '../../util/dynamic-text/HeaderText.jsx';
+import SuisseText from '../../util/dynamic-text/SuisseText.jsx';
+import { BORDER } from '../../../lib/constants.js';
 
 const BootcampScheduleHeader = ({ bootcampSchedule }) => {
 	return (
-		<Stack w='full'>
-			<HeaderText>{bootcampSchedule.title}</HeaderText>
+		<Stack
+			w='full'
+			px={{ base: '16px', md: '10px' }}
+			py={{ base: 2, md: 2 }}
+			borderBottom={BORDER}
+		>
+			<HeaderText>BOOTCAMP SCHEDULE</HeaderText>
+			<SuisseText>{bootcampSchedule.description}</SuisseText>
 		</Stack>
 	);
 };
