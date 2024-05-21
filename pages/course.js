caef57ca6b549c29de11727e75c5f17b148c0903
Md Aminul Haque <61130539+AminulHaqueShihab@users.noreply.github.com>
2024-05-21 13:48:56 +0600

    single course page added

--- pages/course.js
+++ pages/course.js
@@ -3,8 +3,7 @@
 import React from 'react';
 import SingleCoursePage from '../components/course/page/SingleCoursePage';
 
-const course = ({ params }) => {
-	const { id } = params;
+const course = ({}) => {
 	return <SingleCoursePage />;
 };
 
