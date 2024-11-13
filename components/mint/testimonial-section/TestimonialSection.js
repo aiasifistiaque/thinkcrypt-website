019d4c35fe94ea1f3ec7a25bf3aa4830a10405e1
NaimulHaqAbeer <naimulhaqabeer.thinkcrypt@gmail.com>
2024-11-14 02:53:22 +0600

    testimonial section added for mint

--- components/mint/testimonial-section/TestimonialSection.js
+++ components/mint/testimonial-section/TestimonialSection.js
@@ -1,85 +1,98 @@
-import { Avatar, AvatarGroup, Flex, Grid, Image, Text } from '@chakra-ui/react';
-import React from 'react';
-import Column from '../../util/Column';
-import { styles } from '../../../theme/styles';
+import { Avatar, AvatarGroup, Flex, Grid, Image, Text } from "@chakra-ui/react";
+import React from "react";
+import Column from "../../util/Column";
+import { styles } from "../../../theme/styles";
+import { testimonialData } from "../../../data/data";
+import styled from "@emotion/styled";
+import { breakpoints } from "../../lib/constants";
 
 const BORDER = styles.border.light;
 
+const P = styled(Text)`
+	@media (min-width: ${breakpoints.desktop}) {
+		font-size: 1.25rem;
+		line-height: 1.6;
+		max-width: 50vw;
+		fontFamily: "Suisse";
+	}
+`;
+
 const TestimonialSection = () => {
-	return (
-		<Column
-			px={{ base: 6, lg: 10 }}
-			py={10}
-			borderTop={BORDER}
-			borderBottom={BORDER}
-			bg='sectionBg.light'
-		>
-			<Text fontFamily='Suisse' fontSize='2rem' textAlign='center'>
-				See what all the hype is about
-			</Text>
+  return (
+    <Column
+      my="60px"
+      px={{ base: 6, xl: 24 }}
+      py={12}
+      borderTop={BORDER}
+      borderBottom={BORDER}
+      bg="sectionBg.light"
+    >
+      <Text fontFamily="Bebas Neue" fontSize={{ base: '1.5rem', md: '2rem', lg: '3rem' }} textAlign="center">
+        See what all the hype is about
+      </Text>
 
-			<Grid
-				templateColumns={{ base: '1fr', md: '1fr 1fr', lg: '1fr 1fr 1fr' }}
-				px={{ base: '12px', md: '44px', xl: '124px' }}
-				py={{ base: '20px', xl: '40px' }}
-				gap={6}
-				mt={4}
-			>
-				<Flex
-					flexDir='column'
-					bg='white'
-					gap={4}
-					borderRadius={16}
-					overflow='hidden'
-				>
-					<Column bg='#373f51' alignItems='center' p={6} gap={2}>
-						<Text fontSize='1.8rem' fontFamily='Suisse' textColor='white'>
-							14+ Projects
-						</Text>
-						<Text
-							fontSize='1.5rem'
-							textColor='white'
-							fontFamily='Suisse'
-							fontWeight={300}
-						>
-							completed on mint
-						</Text>
-					</Column>
+      <Grid
+        templateColumns={{ base: "1fr", md: "1fr 1fr", xl: "1fr 1fr 1fr" }}
+        p={{ base: 4, md: 16, xl: 28 }}
+        justifyContent="space-between"
+        gap={6}
+        mt={4}
+      >
+        {testimonialData.map((testimonial) => (
+          <Flex
+            flexDir="column"
+            bg="white"
+            gap={4}
+            borderRadius={16}
+            overflow="hidden"
+            key={testimonial?.id}
+          >
+            <Column bg="#373f51" alignItems="center" textAlign='center' p={6} gap={2}>
+              <Text fontFamily='Suisse' fontSize={{base: '1rem', md: '1.1rem',lg: '1.5rem'}} textColor="white" >
+                {testimonial?.heading}
+              </Text>
+              <Text  
+							fontFamily='Suisse' 
+							fontSize={{base: '1rem', md: '1.1rem',lg: '1.4rem'}}        
+                textColor="white"
+                fontWeight={300}
+              >
+                {testimonial?.subHeading}
+              </Text>
+            </Column>
 
-					<Column p={6} gap={6}>
-						<Flex alignItems='center' gap={1}>
-                            <AvatarGroup>
+            <Column p={6} gap={6}>
+              <Flex alignItems="center" gap={1}>
+                <AvatarGroup>
+                  <Avatar
+                    name={`${testimonial?.user}`}
+                    src={`${testimonial?.userImage}`}
+                    shape="full"
+                    size="lg"
+                  />
+                  <Avatar
+                    name={`${testimonial?.company}`}
+                    src={`${testimonial?.companyImage}`}
+                    shape="full"
+                    size="lg"
+                  />
+                </AvatarGroup>
 
-							<Avatar
-								name='Random User'
-								src='https://images.unsplash.com/photo-1531746020798-e6953c6e8e04'
-								shape='full'
-								size='lg'
-							/>
-							<Avatar
-								name='Random User'
-								src='https://images.unsplash.com/photo-1531746020798-e6953c6e8e04'
-								shape='full'
-								size='lg'
-							/>
-                            </AvatarGroup>
+                <Column gap={2} ml={4}>
+                  <P >{testimonial?.user}</P>
+                  <P >{testimonial?.company}</P>
+                </Column>
+              </Flex>
 
-							<Column gap={2} ml={4}>
-								<Text fontSize='1.2rem'>John Doe</Text>
-								<Text fontSize='1.2rem'>Framer</Text>
-							</Column>
-						</Flex>
-
-						<Text fontSize='1.2rem' lineHeight={1.5}>
-							Contra not only helps us find those people with specialized
-							skills, the one-stop-shop solution allows us to manage projects
-							end-to-end.
-						</Text>
-					</Column>
-				</Flex>
-			</Grid>
-		</Column>
-	);
+              <P >
+                {testimonial?.userDescription}
+              </P>
+            </Column>
+          </Flex>
+        ))}
+      </Grid>
+    </Column>
+  );
 };
 
 export default TestimonialSection;
