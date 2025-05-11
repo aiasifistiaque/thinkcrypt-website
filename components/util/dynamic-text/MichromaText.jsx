fe936ea8a926306970a1c84e07647f47745f76ed
Asif Istiaque <asifistiaque.ai@gmail.com>
2025-05-11 14:32:19 +0600

    s

---
+++ components/util/dynamic-text/MichromaText.jsx
@@ -0,0 +1,22 @@
+// import useCustomColors from '@/hooks/useCustomColors';
+
+import { Text, TextProps } from '@chakra-ui/react';
+import React from 'react';
+import { fonts } from '../../../lib/constants';
+import useCustomColors from '../../../hooks/useCustomColors.jsx';
+
+const MichromaText = ({ children, ...props }) => {
+	const { textColor } = useCustomColors();
+	return (
+		<Text
+			fontFamily={fonts.Michroma}
+			color={textColor}
+			fontSize={{ base: '1rem', lg: '1.5rem' }}
+			fontWeight={'400'}
+			{...props}>
+			{children}
+		</Text>
+	);
+};
+
+export default MichromaText;
