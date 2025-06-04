ffa6a576c93d79954e84f4577e2a4f1fbb4f0e6d
Asif Istiaque <asifistiaque.ai@gmail.com>
2025-06-04 14:53:40 +0600

    s

--- components/home/about-us/AboutUsSection.js
+++ components/home/about-us/AboutUsSection.js
@@ -11,7 +11,7 @@ const AboutUsSection = ({ theme }) => {
 			titleTop='We started'
 			title='Our vision'
 			index='02'
-			titleBottom='back in 2023'
+			titleBottom='back in 2017'
 			paragraph='	Today, we build scalable platforms and custom tools that streamline workflows and drive
 					growth. From MVPs to full systems, we handle the tech so businesses can focus on reaching
 					their goals.'>
